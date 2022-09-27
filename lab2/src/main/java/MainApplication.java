9cc13673cb055d11b0acd6ed32dae6b71b5c94cd
Vlad <56757482+TheKront@users.noreply.github.com>
2022-09-27 13:21:51 +0300

    Main maven application

---
+++ lab2/src/main/java/MainApplication.java
@@ -0,0 +1,9 @@
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.print ("Java values:");
+        System.out.print ( 3.1415);
+        System.out.print (", ");
+        System.out.print (15);
+        System.out.println ("(double, char, int).");
+    }
+}
